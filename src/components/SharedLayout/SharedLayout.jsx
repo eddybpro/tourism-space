b4a2cd407295d74a9851b50ced4186c9acf55d42
eddybpro <abdelilahdb154@gmail.com>
2023-09-15 21:58:07 +0100

    fix landmak issue

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -17,7 +17,7 @@ function SharedLayout() {
   return (
     <>
       <div className={menu && !isLarge ? "Head HeadBlur" : "Head"}>
-        <Link to="/" role="complementary" aria-label="space-tourism">
+        <Link to="/" aria-label="space-tourism">
           <img src={Logo} alt="space tourism" className="Head-Logo" />
         </Link>
         {isLarge ? (
